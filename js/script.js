679659d3e599483a6211535d84986021cba554b4
James Rinella <rinellaj2448@gmail.com>
2020-08-14 07:22:42 -0500

    test commit after clone

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var transactions = ["204","210","214","404"];
 $(document).ready(function(){
     
     $('#header nav').append(function(){
+        console.log("Hi");
         var html = "<ul>";
         transactions.forEach(function(item){
             html += ("<li class='index-nav-li'><a href='" + item + ".html'>" + item + "</a></li>")
